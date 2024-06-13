ec97c0fe2f3d280432ac31fb9e49d89b415d023d
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2024-06-14 00:59:20 +0700

    Add test samples and script

---
+++ test/index.test.js
@@ -0,0 +1,46 @@
+import { expect, test } from "bun:test"
+import Dash from "..";
+
+// Made to test the test file itself, lol
+
+async function getResultFromSample(filename) {
+    const src = await Bun.file(`./test/samples/${filename}.dsh`).text();
+    const intepreter = new Dash();
+    return intepreter.intepret(src);
+}
+
+test("Signs and Outputting", async () => {
+    const result = await getResultFromSample("test1");
+
+    expect(result).toBe("54e");
+});
+
+test("Sign Groups", async () => {
+    const result = await getResultFromSample("test2");
+    
+    expect(result).toBe("arb");
+});
+
+test("Duplicators", async () => {
+    const result = await getResultFromSample("test3");
+
+    expect(result).toBe("how");
+})
+
+test("Barriers", async () => {
+    const result = await getResultFromSample("test4");
+
+    expect(result).toBe("69420");
+})
+
+test("Labels", async () => {
+    const result = await getResultFromSample("test5");
+
+    expect(result).toBe(":soo:");
+})
+
+test("Jumps", async () => {
+    const result = await getResultFromSample("test6");
+
+    expect(result).toBe("34");
+})
