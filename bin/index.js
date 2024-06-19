5e2c291cdf89e8d6d74fc00cc59a8b0edae79a6f
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2024-06-19 14:18:19 +0700

    Better Error catching

--- bin/index.js
+++ bin/index.js
@@ -6,18 +6,34 @@ import { argv, exit } from "node:process";
 
 let fileName, input;
 
-argv.forEach((arg, idx) => {
-    if (arg.split(".").pop() === "dsh") {
+for (let idx = 0; idx < argv.length; ++idx) {
+    const arg = argv[idx];
+
+    if (arg.split(".").pop() === "sign") {
         fileName = arg;
     }
     else if (arg === "-i") {
         input = argv[++idx];
     }
-})
+}
 
-if (!fileName) throw Error("File not Specified");
+if (!fileName) {
+    console.log("[Sign]: No Path Specified");
+    exit(1);
+}
 
-const src = readFileSync(fileName, { encoding: "utf-8" });
+let src;
+try {
+    src = readFileSync(fileName, { encoding: "utf-8" });
+}
+catch ({ code }) {
+    if (code === "ENOENT") {
+        console.log("[Sign]: File not Found");
+        exit(1);
+    }
+    else throw new Error("Unknown Error. Please report this issue on GitHub");
+}
+
 const Sign = new SignLang();
 
 if (input) Sign.setInput(input);
